4eb81a9abc753c850f530dcb35f1400702e0bd27
Facundo Perez Schneider <facup200718@gmail.com>
2022-06-07 15:47:52 -0300

    Clase 6 a 9

--- SpringBootHelloApp/src/main/java/com/skillsoft/springboot/App.java
+++ SpringBootHelloApp/src/main/java/com/skillsoft/springboot/App.java
@@ -1,13 +1,14 @@
 package com.skillsoft.springboot;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
