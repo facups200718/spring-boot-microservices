bc5d1fee3c43a095608f13dd33f70fedd77d0652
Facundo Perez Schneider <facup200718@gmail.com>
2022-06-08 17:31:10 -0300

    Clase 11

--- SpringBootHelloApp/src/main/java/com/skillsoft/springboot/App.java
+++ SpringBootHelloApp/src/main/java/com/skillsoft/springboot/App.java
@@ -2,12 +2,16 @@ package com.skillsoft.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(App.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
